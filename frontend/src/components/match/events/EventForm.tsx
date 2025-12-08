056d8bb8a602f534bdb5da70d6c2cf57acb42fc5
David Monreal <davidmonreal@users.noreply.github.com>
2025-12-08 14:40:14 +0100

    Import useRef in EventForm to fix runtime error

--- frontend/src/components/match/events/EventForm.tsx
+++ frontend/src/components/match/events/EventForm.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import {
     User, Users, ArrowUp, ArrowLeftRight,
     Target, Wind, Ban, Hand, Goal as GoalIcon,
